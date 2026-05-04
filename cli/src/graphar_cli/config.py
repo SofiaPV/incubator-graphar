8a2e413a2eb2849fbe9c4ba05768c25a36cc423b
Pobarueva Sofiya <s_ext.spobarueva@m1-rnd-graphar-dev.m1.rnd-processing.local>
2026-05-04 12:43:27 +0300

    add: merge: path to store tmp files is now added to config (.yaml)

--- cli/src/graphar_cli/config.py
+++ cli/src/graphar_cli/config.py
@@ -80,6 +80,24 @@ class GraphArConfig(GrapArBaseConfig):
 
 
 class GraphArMergeConfig(GrapArBaseConfig):
+    tmp_path: Optional[str] = None
+
+    @field_validator('tmp_path', mode='before')
+    @classmethod
+    def validate_tmp_path(cls, v):
+        if v is None or v == '':
+            return ''
+
+        path = Path(v).resolve().absolute()
+
+        if not path.exists():
+            path.mkdir(parents=True, exist_ok=True)
+        elif any(path.iterdir()):
+            msg = f"Warning: Path {path} already exists and contains files."
+            logger.warning(msg)
+        
+        return str(path)
+
     @model_validator(mode='after')
     def check_path(self):
         path = Path(self.path).resolve().absolute()
